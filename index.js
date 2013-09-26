a888f7ddacd41d02fec66db59829309997c41c47
JasonUtt <jutt@jdudesign.com>
2013-09-26 19:30:09 -0400

    Right Items Added

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-	console.log('ready');
+	$(function() {
+    	$( "#sortable" ).sortable();
+    	$( "#sortable" ).disableSelection();
+    });
+var toggleHeight = $('.ui-state-default').height();
+console.log(toggleHeight);
+$('#expand').click(function() {
+  var clicks = $(this).data('clicks');
+  if (clicks) {
+  	$('.content').fadeIn('slow');
+    $(this).siblings('div').slideDown();
+	$('#expand').html('&#9660;');
+  } else {
+  		$('.content').hide();
+     $(this).siblings('div').slideUp();
+	$('#expand').html('&#9650;');
+  }
+  $(this).data("clicks", !clicks);
 });
+$('input').click(function(){
+	$(this).siblings().css('text-decoration','sline-through');
+});
+  });
