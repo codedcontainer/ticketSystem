76edf3bbdcd5d4fc2c6b19ed5c46622f4ebd7af6
jdmagic21 <jutt@jdudesign.com>
2013-09-29 12:38:53 -0400

    Forms & Functions

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 $(document).ready(function()
 {
-	$(function()
+	var config_bool;
+
+	tsk_sortable();
+	tsk_toggle();
+	projectBox();
+	config(); // Returns Bool //Make AJAX request to config.php and make sure everything has been set up
+
+	function tsk_sortable()
 	{
     	$( "#sortable" ).sortable();
     	$( "#sortable" ).disableSelection();
-    });
-	var toggleHeight = $('.ui-state-default').height();
-	$('#expand').click(function()
-	{
-		var clicks = $(this).data('clicks');
-		if (clicks)
+ 	};
+ 	function tsk_toggle()
+ 	{
+		var toggleHeight = $('.ui-state-default').height();
+		$('#expand').click(function()
 		{
-			$('.content').fadeIn('slow');
-			$(this).siblings('div').slideDown();
-			$('#expand').html('&#9660;');
-	  	} 
-	  	else
-	  	{
-			$('.content').hide();
-			$(this).siblings('div').slideUp();
-			$('#expand').html('&#9650;');
-	  	}
-	  	$(this).data("clicks", !clicks);
-	});
-	$('input').click(function(){
-		$(this).siblings().css('text-decoration','sline-through');
-	});
-//Make AJAX request to config.php and make sure everything has been set up
-	var db_test;
-	$.ajax({ url: 'config.php',
-			data:{db_test:1},
-			type: "POST",
-			success: function(data)
+			var clicks = $(this).data('clicks');
+			if (clicks)
 			{
-				console.log(data);
-			}
-	});
+				$('.content').fadeIn('slow');
+				$(this).siblings('div').slideDown();
+				$('#expand').html('&#9660;');
+		  	} 
+		  	else
+		  	{
+				$('.content').hide();
+				$(this).siblings('div').slideUp();
+				$('#expand').html('&#9650;');
+		  	}
+		  	$(this).data("clicks", !clicks);
+		});
+	}	
+	function config()
+	{
+		var dataBool;
+		var db_test;
+		$.ajax({ url: 'config.php',
+				data:{db_test:1},
+				type: "POST",
+				success: function(data)
+				{
+					//return the data value to global variable.
+				}
+		});
+	}
 	function projectBox()
 	{
 		$('.icon-plus-sign').click(function()
@@ -60,5 +70,5 @@ $(document).ready(function()
 			}
 		});
 	}
-	projectBox();
+	
   });
